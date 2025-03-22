26397ce9fd8ff5790a42227b738ba8ed58e5744c
SH <624089195@qq.com>
2025-03-22 18:33:01 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,8 @@ import http from '@/utils/http.js'
 export const hostUrl = process.env.VUE_APP_BASE_API + '/CCSH/'
 
 //列表
-export function requestList() {
-	return http.get(hostUrl + 'list.json')
+export default {
+  requestList() {
+    return http.get(hostUrl + 'list.json')
+  }
 }
